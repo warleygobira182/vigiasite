e36bf1cf9f8101b8cb20076c7932bc8cb8a56a15
warleygobira182 <warleygobira182@gmail.com>
2025-11-08 08:55:16 -0300

    Delete api directory
    deletando o diretorio api inteiro para novos testes

--- api/status.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  res.status(200).json({ 
-    status: 'online',
-    message: 'VigiaSite API funcionando!',
-    timestamp: new Date().toISOString()
-  });
-}
