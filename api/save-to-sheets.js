2399b1ba4605e7d574f148e674b750463fd06046
warleygobira182 <warleygobira182@gmail.com>
2025-11-12 14:00:59 -0300

    Create save-to-sheets.js
    criando save to sheets para listar clientes

---
+++ api/save-to-sheets.js
@@ -0,0 +1,6 @@
+const { GoogleSpreadsheet } = require('google-spreadsheet');
+
+module.exports = async (req, res) => {
+  // Esta função vai salvar cada novo site na planilha
+  // Vamos integrar com o monitor.js
+};
